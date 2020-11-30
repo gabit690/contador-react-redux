1f89bc2bc3c16def202064e062e4b286dd8540a4
gabit <gabit690@gmail.com>
2020-11-30 19:27:55 -0300

    implementación del Contador usando React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css"
 
 function App() {
+
+  const [localCounter, setLocalCounter] = useState(0);
+  const [visitorCounter, setVisitorCounter] = useState(0);
+
+  function incrementLocalCounter() {
+    if (localCounter < 9) setLocalCounter(localCounter+1);
+  }
+
+  function decrementLocalCounter() {
+    if (localCounter > 0) setLocalCounter(localCounter-1);
+  }
+
+  function incrementVisitorCounter() {
+    if (visitorCounter < 9) setVisitorCounter(visitorCounter+1);
+  }
+
+  function decrementVisitorCounter() {
+    if (visitorCounter > 0) setVisitorCounter(visitorCounter-1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor-app">
+      <div className="resultados">
+        <div className="contenedor-contador">
+          <div className="contador">{localCounter}</div>
+          <p>Local</p>
+        </div>
+        <div className="contenedor-contador">
+          <div className="contador">{visitorCounter}</div>
+          <p>Visitante</p>
+        </div>
+      </div>
+      <div className="botonera-app">
+        <div>
+          <p>L</p>
+          <button className="boton-app" onClick={incrementLocalCounter}>Aumentar</button>
+          <button className="boton-app" onClick={decrementLocalCounter}>Disminuir</button>
+        </div>
+        <div>
+          <p>V</p>
+          <button className="boton-app" onClick={incrementVisitorCounter}>Aumentar</button>
+          <button className="boton-app" onClick={decrementVisitorCounter}>Disminuir</button>
+        </div>
+      </div>
     </div>
   );
 }
